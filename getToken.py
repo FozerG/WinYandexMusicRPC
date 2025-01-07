914d6461219ff8ab36bec9396620f8cfd5d68f16
FozerG <dizarmiyt@gmail.com>
2025-01-08 01:12:05 +0300

    using regex for token

--- getToken.py
+++ getToken.py
@@ -3,15 +3,26 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 import sys
+import re
+
+URL_WITH_ACCESS_TOKEN_REGEX = r'https:\/\/music\.yandex\.(?:ru|com|by|kz|ua)\/#access_token=([^&]*)'
+
 
 class CustomWebEnginePage(QWebEnginePage):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.profile().setHttpUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+        )
+        
     def javaScriptConsoleMessage(self, level, message, lineNumber, sourceId):
         pass
-    
+
+
 class TokenWindow(QMainWindow):
     def __init__(self, url, icon_path):
         super().__init__()
-        self.setWindowTitle("Авторизация")
+        self.setWindowTitle("Authorization")
         self.setGeometry(100, 100, 700, 800)
         self.setWindowIcon(QIcon(icon_path))
         
@@ -32,13 +43,15 @@ class TokenWindow(QMainWindow):
 
     def on_url_changed(self, url):
         url_str = url.toString()
-        if "#access_token" in url_str:
-            self.token = url_str.split("=")[1].split("&")[0]
+        match = re.search(URL_WITH_ACCESS_TOKEN_REGEX, url_str)
+        if match:
+            self.token = match.group(1) 
             self.close()
+
 
 def update_token(icon_path):
     app = QApplication(sys.argv)
-    oauth_url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d" # Official link to OAuth Yandex.Music
+    oauth_url = "https://oauth.yandex.ru/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d"  # Official link to OAuth Yandex.Music
     token_window = TokenWindow(oauth_url, icon_path)
     token_window.show()
     app.exec_()
